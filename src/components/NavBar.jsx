161d0a9470d6a3986b189a421898f3d7bfeac873
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-02-14 21:09:07 -0300

    fix: adjusting pages based on user authentication status

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,14 @@ import { NavLink } from "react-router-dom"
 
 import styles from "./NavBar.module.css"
 
+import { useAuthentication } from "../hooks/useAuthentication"
+import { useAuthValue } from "../context/AuthContext"
+
 import { RiHome2Line, RiInformation2Line } from "@remixicon/react"
 
 const NavBar = () => {
+    const { user } = useAuthValue();
+
     return (
         <nav className={styles.navbar}>
             <NavLink to="/" className={styles.brand}>
@@ -13,22 +18,44 @@ const NavBar = () => {
 
             <ul className={styles.links_list}>
                 <li>
-                    <NavLink to="/" className={({isActive}) => (isActive ? styles.active : "")}>
+                    <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>
                         Home
                     </NavLink>
                 </li>
+
+                {/* show options if user is NOT logged in */}
+                {!user && (
+                    <>
+                        <li>
+                            <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Login
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Register
+                            </NavLink>
+                        </li>
+                    </>
+                )}
+
+                {/* show options if user IS logged in */}
+                {user && (
+                    <>
+                        <li>
+                            <NavLink to="/posts/create" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Post
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/dashboard" className={({ isActive }) => (isActive ? styles.active : "")}>
+                                Dashboard
+                            </NavLink>
+                        </li>
+                    </>
+                )}
                 <li>
-                    <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>
-                        Login
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>
-                        Register
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : "")}>
                         About
                     </NavLink>
                 </li>
