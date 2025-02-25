79aa19eddbf8f7dd771480513ecabbdb1459cb13
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-02-24 21:11:29 -0300

    fix: adjusting flippy cards

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,16 +3,31 @@ import styles from "./About.module.css";
 import { Link } from "react-router-dom";
 import { Grid2 } from "@mui/material";
 import ReactCardFlip from "react-card-flip";
-import { ArrowBigRight } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const About = () => {
     const [flippedCards, setFlippedCards] = useState({});
+
+    const cards = [
+        {
+            front: "Tecnologias usadas",
+            back: "Teste 1"
+        },
+        {
+            front: "Teste de card",
+            back: "Teste 2"
+        },
+        {
+            front: "Teste de card outro",
+            back: "Teste 3"
+        }
+    ]
 
     // transformando somente o card que foi clicado
     const handleFlip = (index) => {
         setFlippedCards((prevState) => ({
             ...prevState,
-            [index]: !prevState[index], 
+            [index]: !prevState[index],
         }));
     };
 
@@ -27,25 +42,29 @@ const About = () => {
             </p>
 
             <Grid2 container spacing={5} sx={{ marginTop: "3rem" }}>
-                {[1, 2, 3].map((index) => (
-                    <Grid2 item xs={12} md={4} key={index}>
-                        <ReactCardFlip isFlipped={!!flippedCards[index]} flipDirection="horizontal">
-                            
-                            <div className={styles.box}>
-                                <p className={styles.boxText}>Tecnologias utilizadas</p>
-                                <button onClick={() => handleFlip(index)}>
-                                    <ArrowBigRight />
-                                </button>
-                            </div>
+                {cards.map((card, index) => {
+                    return (
+                        <Grid2 xs={12} md={4} key={index}>
+                            <ReactCardFlip isFlipped={!!flippedCards[index]} flipDirection="horizontal">
 
-                            
-                            <div className={styles.box}>
-                                <p>Informação do Card {index}</p>
-                                <button onClick={() => handleFlip(index)}>Voltar</button>
-                            </div>
-                        </ReactCardFlip>
-                    </Grid2>
-                ))}
+                                <div className={styles.box}>
+                                    <p className={styles.boxText}>{card.front}</p>
+                                    <button className={styles.front} onClick={() => handleFlip(index)}>
+                                        <ChevronRight size={35} />
+                                    </button>
+                                </div>
+
+
+                                <div className={styles.box}>
+                                    <p className={styles.boxText}>{card.back}</p>
+                                    <button className={styles.back} onClick={() => handleFlip(index)}>
+                                        <ChevronLeft size={41} />
+                                    </button>
+                                </div>
+                            </ReactCardFlip>
+                        </Grid2>
+                    )
+                })}
             </Grid2>
 
             <Link to="/posts/create" className="botao">
