dab9343c2b9a3d5bd5092755a9251565a29c71bc
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-02-27 21:47:45 -0300

    feat: adding posts

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -2,8 +2,11 @@ import styles from "./CreatePost.module.css"
 
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { Captions, Image, Plus, Tag, Text } from "lucide-react"
+
 import { useAuthValue } from "../../context/AuthContext"
-import { Captions, Image, Tag, Text } from "lucide-react"
+import { userInsertDocument } from "../../hooks/useInsertDocument"
+import { RiErrorWarningLine } from "@remixicon/react"
 
 const CreatePost = () => {
     const [title, setTitle] = useState("");
@@ -12,8 +15,30 @@ const CreatePost = () => {
     const [tags, setTags] = useState([]);
     const [formError, setFormError] = useState("");
 
+    const {user} = useAuthValue();
+
+    const {insertDocument, response} = userInsertDocument();
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        setFormError("");
+
+        // validate image URL
+
+        // creating tag array
+
+        // check values
+
+        insertDocument({
+            title,
+            image, 
+            body,
+            tags,
+            uid: user.uid,
+            createdBy: user.displayName
+        })
+
+        // redirect to home page
     }
 
     return (
@@ -27,29 +52,39 @@ const CreatePost = () => {
                         <Captions className={styles.icon} />
                         <span className={styles.title}>Título</span>
                     </div>
-                    <input type="text" name="title" required placeholder="Pense num bom título..." onChange={(e) => setTitle(e.target.value)} value={title} className={styles.form} />
+                    <input type="text" name="title" required placeholder="Pense num bom título..." onChange={(e) => setTitle(e.target.value)} value={title} className={styles.form} autoComplete="off" />
                 </label>
                 <label className={styles.sep}>
                     <div style={{display:"flex", alignItems:"center"}}>
                         <Image className={styles.icon} />
                         <span className={styles.title}>Imagem</span>
                     </div>
-                    <input type="text" name="image" required placeholder="Insira uma imagem que representa o seu post" onChange={(e) => setImage(e.target.value)} value={image} className={styles.form} />
+                    <input type="text" name="image" required placeholder="Insira uma imagem que representa o seu post" onChange={(e) => setImage(e.target.value)} value={image} className={styles.form} autoComplete="off" />
                 </label>
                 <label className={styles.sep}>
                     <div style={{display:"flex", alignItems:"center"}}>
                         <Text className={styles.icon} />
                         <span className={styles.title}>Conteúdo</span>
                     </div>
-                    <textarea name="body" required placeholder="Insira o conteúdo do post" onChange={(e) => setBody(e.target.value)} value={body} id="createPost" className={styles.area}></textarea>
+                    <textarea name="body" required placeholder="Insira o conteúdo do post" onChange={(e) => setBody(e.target.value)} value={body} id="createPost" className={styles.area} autoComplete="off"></textarea>
                 </label>
                 <label className={styles.sep}>
                     <div style={{display:"flex", alignItems:"center"}}>
                         <Tag className={styles.icon} />
                         <span className={styles.title}>Tags</span>
                     </div>
-                    <input type="text" name="tags" required placeholder="Insira as tags, separadas por vírgulas" onChange={(e) => setTags(e.target.value)} value={tags} className={styles.form} />
+                    <input type="text" name="tags" required placeholder="Insira as tags, separadas por vírgulas" onChange={(e) => setTags(e.target.value)} value={tags} className={styles.form} autoComplete="off" />
                 </label>
+
+                {!response.loading && 
+                <button className={styles.btnCreate}>
+                    <Plus size={50} />
+                </button>}
+                {response.loading && <button className="btn" disabled>Aguarde...</button>}
+
+                {response.error && <p className="error">
+                <RiErrorWarningLine style={{marginRight:"10px", color:"red"}} />
+                {response.error}</p>}
             </form>
         </div>
     )
