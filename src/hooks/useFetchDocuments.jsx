264804c5be39ffbd446c2a58799e8c6592df10cc
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-03-06 21:35:45 -0300

    feat: implementing fetch documents

---
+++ src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import { collection, query, orderBy, onSnapshot, where, QuerySnapshot } from "firebase/firestore"
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+    const [documents, setDocuments] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    // deal with memory leak
+    const [cancelled, setCancelled] = useState(false)
+
+    useEffect(() => {
+        async function loadData() {
+            if (cancelled) return
+
+            // loading data
+            setLoading(true)
+
+            const collectionRef = collection(db, docCollection);
+
+            try {
+                let q;
+
+                q = query(collectionRef, orderBy("createdAt", "desc"));
+
+                onSnapshot(q, (QuerySnapshot) => {
+                    setDocuments(
+                        QuerySnapshot.docs.map((doc) => ({
+                            id: doc.id,
+                            ...doc.data(),
+                        }))
+                    )
+                });
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setError(error.message)
+
+                setLoading(false);
+            }
+        }
+
+        loadData();
+    }, [docCollection, search, uid, cancelled])
+
+    useEffect(() => {
+        return () => setCancelled(true)
+    }, [])
+
+    return { documents, error, loading }
+}
