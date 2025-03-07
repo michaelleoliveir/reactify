b33a76134552c6d43d9f01dba5822c1eabf35b7c
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-03-06 21:16:20 -0300

    feat: styling home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,37 @@
 import styles from "./Home.module.css"
 
+import { useNavigate, Link } from "react-router-dom"
+import { useState } from "react"
+import { Frown, Search } from "lucide-react";
+
 export const Home = () => {
+    const [query, setQuery] = useState("");
+    const [posts] = useState([]);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>Home</div>
+        <div className={styles.home}>
+            <form onSubmit={handleSubmit} className={styles.search}>
+                <input type="text" placeholder="Ou busque por tags..." onChange={(e) => setQuery(e.target.value)} />
+                <button>
+                    <Search />
+                </button>
+            </form>
+
+            {/* showing posts */}
+            <div className={styles.section}>
+                
+                {posts && posts.length === 0 && (
+                    <div className={styles.footer}>
+                        <Frown size={50} className={styles.frown} />
+                        <p>Não foram encontrados posts</p>
+                        <Link to="/posts/create" className={styles.linkTo}>Criar primeiro post</Link>
+                    </div>
+                )}
+            </div>
+        </div>
     )
 }
