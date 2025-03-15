57356dd1bda77751a80019b3126b11efc0f9c930
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-03-15 16:31:21 -0300

    feat: styling individual posts

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,21 +1,35 @@
 import styles from "./Post.module.css"
 
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { useFetchDocument } from "../../hooks/useFetchDocument";
+import { ChevronLeft } from "lucide-react";
 
 const Post = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const { document: post, loading } = useFetchDocument("posts", id)
 
     return (
-        <div>
+        <div className={styles.post}>
             {loading && (
                 <p>Carregando post</p>
             )}
             {post && (
-                <>
-                    <h1>{post.title}</h1>
-                </>
+                <div className={styles.details}>
+                    <div className={styles.heading}>
+                        <Link to="/">
+                            <ChevronLeft className={styles.icon} size={25} />
+                        </Link>
+                        <h1>{post.title}</h1>
+                    </div>
+                    <img src={post.image} alt={post.title} />
+                    <p className={styles.body}>{post.body}</p>
+                    <h3>Este post trata sobre:</h3>
+                    <div className={styles.tags}>
+                        {post.tagsArray.map((map) => (
+                            <p key={map}><span>#</span>{map}</p>
+                        ))}
+                    </div>
+                </div>
             )}
         </div>
     )
