7ba994a1317e634a515077b5dcabdf3eb5e94813
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-03-15 12:43:34 -0300

    feat: styling search page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -5,23 +5,31 @@ import { useQuery } from "../../hooks/useQuery"
 
 import PostDetail from "../../components/PostDetail"
 import { Link } from "react-router-dom"
+import { CircleAlert } from "lucide-react"
 
 const Search = () => {
     const query = useQuery()
     const search = query.get("q")
 
-    const {documents: posts} = useFetchDocuments("posts", search)
+    const { documents: posts } = useFetchDocuments("posts", search)
 
     return (
-        <div>
-            <h2>Search</h2>
+        <div className={styles.search_module}>
             <div>
-                {posts && posts.length === 0 && (
-                    <>
+                {posts && posts.length !== 0 ? (
+                    <div className={styles.heading}>
+                        <h2>Exibindo resultados:</h2>
+                        <p><span>#</span>{search}</p>
+                    </div>
+                ) : (
+                    <div className={styles.error}>
+                        <CircleAlert size={60} strokeWidth={1.5} />
                         <p>Não foram encontrados posts a partir da sua busca...</p>
-                        <Link to="/home" />
-                    </>
+                        <Link to="/" className={styles.goHome}>Voltar para Home</Link>
+                    </div>
                 )}
+            </div>
+            <div>
                 {posts && posts.map((post) => (
                     <PostDetail key={post.id} post={post} />
                 ))}
