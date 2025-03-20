f9cb66060887e3f312b0c72a442793cc4ac7632b
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-03-19 21:51:22 -0300

    feat: initializing dashboard component

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext"
 import styles from "./Dashboard.module.css"
 
 const Dashboard = () => {
+    const {user} = useAuthValue();
+    const uid = user.uid
+
+    //posts do usuário
+    const posts = [];
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <p>Gerencie os seus posts</p>
+            {posts && posts.length === 0 ? (
+                <div>
+                    <p>Não foram encontrados</p>
+                    <Link to="/posts/create">Criar post</Link>
+                </div>
+            ) : (
+                <div>
+                    
+                </div>
+            )}
         </div>
     )
 }
