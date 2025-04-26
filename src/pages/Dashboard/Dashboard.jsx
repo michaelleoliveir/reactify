b0ed6797afb75a481c8898b0b1a70e538a811a0d
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-04-25 21:35:04 -0300

    feat: estilizando o dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -9,14 +9,16 @@ const Dashboard = () => {
 
     const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
-    const deleteDocument = (id) => {
+    const deleteDocument = (id) => { }
 
+    if (loading) {
+        return <p>Carregando...</p>
     }
 
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Gerencie os seus posts</p>
+        <div className={styles.dashboard}>
+            <h1 className={styles.title}>Dashboard</h1>
+            <p className={styles.subtitulo}>Gerencie os seus posts</p>
             {posts && posts.length === 0 ? (
                 <div>
                     <p>Não foram encontrados</p>
@@ -24,24 +26,22 @@ const Dashboard = () => {
                 </div>
             ) : (
                 <>
-                    <div>
-                        <span>Título</span>
-                        <span>Ações</span>
-                    </div>
-
-                    {posts && posts.map((post) => (
-                        <div key={post.id}>
-                            <p>{post.title}</p>
-
-                            <div>
-                                <Link to={`/posts/${post.id}`}>Ver</Link>
-
-                                <Link to={`/posts/edit/${post.id}`}></Link>
-
-                                <button onClick={() => deleteDocument(post.id)}>Delete</button>
+                    <div style={{ marginTop: "10px" }}>
+                        {posts && posts.map((post) => (
+                            <div className={styles.cards} key={post.id}>
+                                <p className={styles.post_title}>{post.title}</p>
+                                <div className={styles.botoes_edit}>
+                                    <div className={`${styles.edit_opt} ${styles.ver}`}>
+                                        <Link to={`/posts/${post.id}`}>Ver</Link>
+                                    </div>
+                                    <div className={`${styles.edit_opt} ${styles.editar}`}>
+                                        <Link to={`/posts/edit/${post.id}`}>Editar</Link>
+                                    </div>
+                                    <div className={`${styles.edit_opt} ${styles.deletar}`}><button onClick={() => deleteDocument(post.id)}>Delete</button></div>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </>
             )}
         </div>
